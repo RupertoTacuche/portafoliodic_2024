d7bf2a405cc8cda4917180d2d14fb10ae2cc7333
@RupertoTacuche <murcio2490@gmail.com>
2025-01-06 12:06:26 -0600

    portafolio_preliminar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,36 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className="text-white bg-gray-d py-20 px-3">
+        <article className="grid gap-6 max-w-[1200px] mx-auto sm:grid-cols-2">
+            <section>
+                <div>
+                    <img src="/images/arduinofondonegro.jpeg" alt=" " />
+                </div>
+                <div className="flex gap-4 py-6"> 
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+
+            </section>
+
+            <section className="grid grid-cols-2">
+                <article className="grid gap-6">
+                    <a href="#" >Home</a>
+                    <a href="#" >About</a>
+                    <a href="#" >Work</a>
+                    <a href="#" >Process</a>
+                </article>
+                <article className="grid gap-6">
+                    <a href="#" >Store</a>
+                    <a href="#" >Blog</a>
+                    <a href="#" >Reading List</a>
+                   
+                </article>
+            </section>
+        </article>
+    </section>
   )
 }
 
